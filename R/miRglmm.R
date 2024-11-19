dba25485c13da4bd6a265d5cef9cc5328689d818
ANDREA MARIE BERRY <aberry3@bhc0135.bluehive.circ.private>
2024-11-19 14:10:16 -0500

    add poisson option to miRglmm

--- R/miRglmm.R
+++ R/miRglmm.R
@@ -1,18 +1,17 @@
-miRglmm <- function(se, col_group,
+library(tidyverse)
+library(reshape2)
+library(lme4)
+library(SummarizedExperiment)
+library(foreach)
+library(doParallel)
+miRglmm <- function(se, col_group = c(rep("A", 19), rep("B",20)), family="NB",
                     min_med_lcpm = -1, ncores = 1, adjust_var=NA){
   
-  if(!"miRNA" %in% names(rowData(se))){
-    stop("rowData(se) must include a variable named miRNA containing unique miRNA IDs")
-  }
-  if(!"uniqueSequence" %in% names(rowData(se))){
-    stop("rowData(se) must include a variable named uniqueSequence containing unique sequence/isomiR IDs")
-  }
   if (is.na(adjust_var)){
     adjust_var=rep(NA, length(col_group))
   }
-  
     
-  ## for each miRNA (parallelized over miRNA if ncores>1)
+  ## for each miRNA (this could be parallelized)
   uniq_miRNA = unique(rowData(se)$miRNA)
   total_counts=colSums(assay(se))
   if (ncores==1){
@@ -44,12 +43,12 @@ miRglmm <- function(se, col_group,
     f1=0
     f1_sub=0
     if (run_miRNA==1){
-
+    ## format data to fit glmer.nb models
     data_wide = as.data.frame(as.matrix(Y_all_sub))
     colnames(data_wide) = Y_seq_labels_sub
     sample_labels = rownames(Y_all_sub)
     data_wide = cbind(col_group, adjust_var, total_counts, sample_labels, data_wide)
-    data_long = reshape2::melt(data_wide, 
+    data_long = melt(data_wide, 
                      id.vars = c("col_group", "adjust_var", "total_counts", "sample_labels"),
                      variable.name = "sequence", 
                      value.name = "count")
@@ -60,6 +59,7 @@ miRglmm <- function(se, col_group,
       Formula_full="count ~col_group + adjust_var + offset(log(total_counts/1e4)) + (1+col_group|sequence) + (1|sample_labels)"
       Formula_sub="count ~col_group + adjust_var + offset(log(total_counts/1e4)) + (1|sequence) + (1|sample_labels)"
     }
+    if (family=="NB"){
     tryCatch({
       f1 = glmer.nb(Formula_full, 
                   data=data_long, 
@@ -74,6 +74,22 @@ miRglmm <- function(se, col_group,
                                               tolPwrss = 1e-3, 
                                               optCtrl=list(maxfun=2e5))))
   }, error = function(e){cat("ERROR :", uniq_miRNA[ind3])})
+    } else if (family=="poisson"){
+      tryCatch({
+        f1 = glmer(Formula_full, 
+                      data=data_long, family="poisson",
+                      control=(glmerControl(optimizer="bobyqa", 
+                                            tolPwrss = 1e-3, 
+                                            optCtrl=list(maxfun=2e5))))
+      }, error = function(e){cat("ERROR :", uniq_miRNA[ind3])})
+      tryCatch({  
+        f1_sub = glmer(Formula_sub, 
+                          data=data_long, family="poisson",
+                          control=(glmerControl(optimizer="bobyqa", 
+                                                tolPwrss = 1e-3, 
+                                                optCtrl=list(maxfun=2e5))))
+      }, error = function(e){cat("ERROR :", uniq_miRNA[ind3])})
+    }
     f1_list[[uniq_miRNA[ind3]]]=f1
     f1_sub_list[[uniq_miRNA[ind3]]]=f1_sub
     }
@@ -84,6 +100,7 @@ miRglmm <- function(se, col_group,
   } else {
     print('running parallel')
     fits_full=foreach(ind3=1:length(uniq_miRNA), .packages=c("tidyverse", "reshape2", "lme4", "SummarizedExperiment")) %dopar% {
+      #cat(uniq_miRNA[ind3], "\n")
       f1_list=list()
       ## subset sequences that map to the miRNA
       idx = which(rowData(se)$miRNA == uniq_miRNA[ind3])
@@ -106,12 +123,12 @@ miRglmm <- function(se, col_group,
       }
       if (run_miRNA==1){
       
-
+      ## format data to fit glmer.nb models
       data_wide = as.data.frame(as.matrix(Y_all_sub))
       colnames(data_wide) = Y_seq_labels_sub
       sample_labels = rownames(Y_all_sub)
       data_wide = cbind(col_group, adjust_var, total_counts, sample_labels, data_wide)
-      data_long = reshape2::melt(data_wide, 
+      data_long = melt(data_wide, 
                        id.vars = c("col_group", "adjust_var", "total_counts", "sample_labels"),
                        variable.name = "sequence", 
                        value.name = "count")
@@ -120,7 +137,8 @@ miRglmm <- function(se, col_group,
         Formula_full="count ~col_group + offset(log(total_counts/1e4)) + (1+col_group|sequence) + (1|sample_labels)"
          } else {
         Formula_full="count ~col_group + adjust_var + offset(log(total_counts/1e4)) + (1+col_group|sequence) + (1|sample_labels)"
-       }
+         }
+      if (family=="NB"){
       tryCatch({
         f1 = glmer.nb(Formula_full, 
                       data=data_long, 
@@ -128,13 +146,21 @@ miRglmm <- function(se, col_group,
                                             tolPwrss = 1e-3, 
                                             optCtrl=list(maxfun=2e5))))
       }, error = function(e){cat("ERROR :", uniq_miRNA[ind3])})
-
+      } else if (family=="poisson"){
+        tryCatch({
+          f1 = glmer(Formula_full, 
+                        data=data_long, family="poisson",
+                        control=(glmerControl(optimizer="bobyqa", 
+                                              tolPwrss = 1e-3, 
+                                              optCtrl=list(maxfun=2e5))))
+        }, error = function(e){cat("ERROR :", uniq_miRNA[ind3])})
+      }
       f1_list[[uniq_miRNA[ind3]]]=f1
       }
       return(f1_list)
     }
     fits_red=foreach(ind3=1:length(uniq_miRNA), .packages=c("tidyverse", "reshape2", "lme4", "SummarizedExperiment")) %dopar% {
-
+      #cat(uniq_miRNA[ind3], "\n")
       f1_list=list()
       ## subset sequences that map to the miRNA
       idx = which(rowData(se)$miRNA == uniq_miRNA[ind3])
@@ -157,11 +183,12 @@ miRglmm <- function(se, col_group,
       }
       if (run_miRNA==1){
       
+      ## format data to fit glmer.nb models
       data_wide = as.data.frame(as.matrix(Y_all_sub))
       colnames(data_wide) = Y_seq_labels_sub
       sample_labels = rownames(Y_all_sub)
       data_wide = cbind(col_group, adjust_var, total_counts, sample_labels, data_wide)
-      data_long = reshape2::melt(data_wide, 
+      data_long = melt(data_wide, 
                        id.vars = c("col_group", "adjust_var", "total_counts", "sample_labels"),
                        variable.name = "sequence", 
                        value.name = "count")
@@ -171,6 +198,7 @@ miRglmm <- function(se, col_group,
       } else {
         Formula_sub="count ~col_group + adjust_var + offset(log(total_counts/1e4)) + (1|sequence) + (1|sample_labels)"
       }
+      if (family=="NB"){
       tryCatch({
 
         f1_sub = glmer.nb(Formula_sub, 
@@ -180,6 +208,16 @@ miRglmm <- function(se, col_group,
                                                 optCtrl=list(maxfun=2e5))))
         
       }, error = function(e){cat("ERROR :", uniq_miRNA[ind3])})
+      } else if (family=="poisson"){
+        tryCatch({
+          f1_sub = glmer(Formula_sub, 
+                            data=data_long, family="poisson",
+                            control=(glmerControl(optimizer="bobyqa", 
+                                                  tolPwrss = 1e-3, 
+                                                  optCtrl=list(maxfun=2e5))))
+          
+        }, error = function(e){cat("ERROR :", uniq_miRNA[ind3])})
+      }
       f1_list[[uniq_miRNA[ind3]]]=f1_sub
       }
       return(f1_list)
